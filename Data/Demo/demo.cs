1d5d8da22eab2ee1c2cd04bf9933097159ffcbdf
Matt <mjzent@csbsju.edu>
2019-10-05 18:48:21 -0500

    demo data for all the super heros in fargo

--- Data/Demo/demo.cs
+++ Data/Demo/demo.cs
@@ -33,6 +33,21 @@ namespace MyHero.Data.Demo
             Requestor r2 = new Requestor { User = u2, Longitude = -96.7870939, Latitude = 46.8778118, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
             Context.Requestor.Add(r2);
 
+            String[] names = { "Bob", "Alice", "Tom", "Cindy", "Mohammed", "Alex", "Charles", "Dale", "Daniel", "Jonathon", "Johnson", "Akshay" };
+            for (int i = 0; i < 50; i++)
+            {
+                Random rnd = new Random();
+                String name = names[rnd.Next(0, names.Length - 1)] + " " + names[rnd.Next(0, names.Length - 1)];
+                ApplicationUser ru = new ApplicationUser { UserName = name, Email = name + "@gmail.com" };
+                Context.User.Add(ru);
+                Hero rh = new Hero { User = ru, Longitude = -96.79980339999998, Latitude = 46.8871863, Location = "Fargo, ND, USA", Phone = "701-321-4567", Tags = "Fire Fighter;Spider Man;Deadpool", Description = "Hi, I'm Bilbo Baggins. I'm from Middle Earth and am new to Fargo. Looking to do some community service in the area.", Radius = 50 };
+                Context.Hero.Add(rh);
+                name = names[rnd.Next(0, names.Length - 1)] + " " + names[rnd.Next(0, names.Length - 1)];
+                ApplicationUser ru2 = new ApplicationUser { UserName = name, Email = name + "@gmail.com" };
+                Context.User.Add(ru2);
+                Requestor rr = new Requestor { User = ru2, Longitude = -96.7970939, Latitude = 46.8678118, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
+                Context.Requestor.Add(rr);
+            }
 
 
             Context.SaveChanges();
