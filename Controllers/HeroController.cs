ccc39d46ba63d90ac87f2dc52016d45af79c5953
Colton Lathrop <colathro@microsoft.com>
2019-10-05 19:29:54 -0500

    controller shizit

---
+++ Controllers/HeroController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using MyHero.Data;
+using Microsoft.EntityFrameworkCore;
+using MyHero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHero.Controllers
+{
+    public class HeroController 
+    {
+        private ApplicationDbContext dbcontext;
+
+        public HeroController(ApplicationDbContext _dbcontext)
+        {
+            dbcontext = _dbcontext;
+        }
+
+        public ICollection<Hero> GetHeros(ApplicationUser _user)
+        {
+            // Fetch User Location
+
+            // Custom query to return heros in radius
+
+            //return heros
+
+            throw new NotImplementedException();
+        }
+    }
+}
