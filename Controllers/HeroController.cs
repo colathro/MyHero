e1d570c1ff32f1db9e13bec6ff7f4a8911e73789
Matt <mjzent@csbsju.edu>
2019-10-05 20:34:34 -0500

    cool sql

--- Controllers/HeroController.cs
+++ Controllers/HeroController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyHero.Controllers
 {
-    public class HeroController 
+    public class HeroController
     {
         private ApplicationDbContext dbcontext;
 
@@ -21,12 +21,13 @@ namespace MyHero.Controllers
         public ICollection<Hero> GetHeros(ApplicationUser _user)
         {
             // Fetch User Location
-
+            double lat = _user.Requestor.Latitude;
+            double lng = _user.Requestor.Longitude;
+            FormattableString sql = $@"with c as (Select *, (Longitude - {lng}) * COS(({lat} + Latitude) / 2) as A, Latitude - {lat} as B From Hero Where Latitude < {lat} + 0.0253 and Latitude > {lat} - 0.0253 and Longitude < {lng} + 0.0371 and Longitude > {lng} - 0.0371) Select * From C Where SQRT(A * A + B * B) * 3958.8 < Radius";
+            return dbcontext.Hero.FromSqlInterpolated(sql).ToList();
             // Custom query to return heros in radius
 
             //return heros
-
-            throw new NotImplementedException();
         }
     }
 }
--- Data/Demo/demo.cs
+++ Data/Demo/demo.cs
@@ -18,11 +18,11 @@ namespace MyHero.Data.Demo
         {
             ApplicationUser u1 = new ApplicationUser { UserName = "Bob Saget", Email = "BobSaget@gmail.com" };
             Context.User.Add(u1);
-            Requestor r1 = new Requestor { User = u1, Longitude = -96.7870939, Latitude = 46.8778118, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
+            Requestor r1 = new Requestor { User = u1, Longitude = -1, Latitude = 1, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
             Context.Requestor.Add(r1);
             ApplicationUser u3 = new ApplicationUser { UserName = "Bilbo Baggins", Email = "BilboBaggins@gmail.com" };
             Context.User.Add(u3);
-            Hero h1 = new Hero { User = u3, Longitude = -96.78980339999998, Latitude = 46.8771863, Location = "Fargo, ND, USA", Phone = "701-123-4567", Tags = "Wizard;Spider Man;Deadpool", Description = "Hi, I'm Bilbo Baggins. I'm from Middle Earth and am new to Fargo. Looking to do some community service in the area.", Radius = 50 };
+            Hero h1 = new Hero { User = u3, Longitude = -1, Latitude = 1, Location = "Fargo, ND, USA", Phone = "701-123-4567", Tags = "Wizard;Spider Man;Deadpool", Description = "Hi, I'm Bilbo Baggins. I'm from Middle Earth and am new to Fargo. Looking to do some community service in the area.", Radius = 50 };
             Context.Hero.Add(h1);
             Photos p1 = new Photos { Image = "Spiderman" };
             Context.Photos.Add(p1);
@@ -30,7 +30,7 @@ namespace MyHero.Data.Demo
             Context.HeroPhotos.Add(hp1);
             ApplicationUser u2 = new ApplicationUser { UserName = "Paul Hewson", Email = "Bono@gmail.com" };
             Context.User.Add(u2);
-            Requestor r2 = new Requestor { User = u2, Longitude = -96.7870939, Latitude = 46.8778118, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
+            Requestor r2 = new Requestor { User = u2, Longitude = -1, Latitude = 1, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
             Context.Requestor.Add(r2);
 
             String[] names = { "Bob", "Alice", "Tom", "Cindy", "Mohammed", "Alex", "Charles", "Dale", "Daniel", "Jonathon", "Johnson", "Akshay" };
@@ -40,12 +40,12 @@ namespace MyHero.Data.Demo
                 String name = names[rnd.Next(0, names.Length - 1)] + " " + names[rnd.Next(0, names.Length - 1)];
                 ApplicationUser ru = new ApplicationUser { UserName = name, Email = name + "@gmail.com" };
                 Context.User.Add(ru);
-                Hero rh = new Hero { User = ru, Longitude = -96.79980339999998, Latitude = 46.8871863, Location = "Fargo, ND, USA", Phone = "701-321-4567", Tags = "Fire Fighter;Spider Man;Deadpool", Description = "Hi, I'm Bilbo Baggins. I'm from Middle Earth and am new to Fargo. Looking to do some community service in the area.", Radius = 50 };
+                Hero rh = new Hero { User = ru, Longitude = -1, Latitude = 1, Location = "Fargo, ND, USA", Phone = "701-321-4567", Tags = "Fire Fighter;Spider Man;Deadpool", Description = "Hi, I'm Bilbo Baggins. I'm from Middle Earth and am new to Fargo. Looking to do some community service in the area.", Radius = 50 };
                 Context.Hero.Add(rh);
                 name = names[rnd.Next(0, names.Length - 1)] + " " + names[rnd.Next(0, names.Length - 1)];
                 ApplicationUser ru2 = new ApplicationUser { UserName = name, Email = name + "@gmail.com" };
                 Context.User.Add(ru2);
-                Requestor rr = new Requestor { User = ru2, Longitude = -96.7970939, Latitude = 46.8678118, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
+                Requestor rr = new Requestor { User = ru2, Longitude = -1, Latitude = 1, Location = "Black Coffee and Waffle Bar, 2nd Avenue North, Fargo, ND, USA", Description = "Hi, my son has been diagnosted with cancer and needs a hero to improve his spirits. He would love it if he got to meet a real life firefighter." };
                 Context.Requestor.Add(rr);
             }
 
